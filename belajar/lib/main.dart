b25fdd4440cbd702707bd6b8368adfa253b72352
Fedrick Siagian <superiorsiagian@gmail.com>
2021-06-17 14:31:17 +0700

    simple first page

--- belajar/lib/main.dart
+++ belajar/lib/main.dart
@@ -4,25 +4,82 @@ void main() {
   runApp(MyApp());
 }
 
+class DetailScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Center(
+                child: Text(
+                  'Farm House Lembang',
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.calendar_today),
+                      SizedBox(height: 10),
+                      Text('Open Everyday')
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.watch),
+                      SizedBox(height: 10),
+                      Text('09.00-20.00'),
+                    ],
+                  ),
+                  Column(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(height: 10),
+                      Text('Rp. 25.000'),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.all(15),
+              child: Text(
+                'Berada dijalur utama Bandung-Lembang, Farm House menjadi objek'
+                ' yang tidak pernah sepi pengunjung. Selain karena letaknya strategis,'
+                ' kawasan ini juga menghadirkan wisata nuansa khas Eropa. Semua itu'
+                ' diterapkan dalam bentuk spot swafoto Instagrammable.',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 16,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Wisata Bandung',
+      theme: ThemeData(),
+      home: DetailScreen(),
     );
   }
 }
